4bac1c58ee21cda4c1e8609368ef00d329b8e6da
Akaiiny <134936445+Akaiiny@users.noreply.github.com>
2026-03-16 00:55:27 +0400

    Delete pages directory

--- pages/page_locators.py
+++
@@ -1,2 +0,0 @@
-class PlaywrightPageLocators:
-    HEADER = "header.hero h1.hero__title"
--- pages/playwright_home_page.py
+++
@@ -1,9 +0,0 @@
-import allure
-from pages.base_page import BasePage
-
-
-class PlaywrightHomePage(BasePage):
-
-    @allure.step("Проверяем заголовок страницы на '{expected_text}'")
-    def verify_header_correctness(self, header_locator: str) ->  None:
-        header = self.find_element(header_locator)
